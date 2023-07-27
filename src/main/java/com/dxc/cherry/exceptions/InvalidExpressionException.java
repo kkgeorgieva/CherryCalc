5ef9ef08a0b8a13ad7608092f297b8f97262bb07
Kamelia Toteva <kamelia.toteva@dxc.com>
2023-07-27 16:38:17 +0300

    Initial add of Exception classes

---
+++ src/main/java/com/dxc/cherry/exceptions/InvalidExpressionException.java
@@ -0,0 +1,12 @@
+package com.dxc.cherry.exceptions;
+
+public class InvalidExpressionException extends Exception {
+
+	public InvalidExpressionException(String errorMessage) {
+		super(errorMessage);
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3159034685771832076L;
+}
---
+++ src/main/java/com/dxc/cherry/exceptions/InvalidOperationException.java
@@ -0,0 +1,13 @@
+package com.dxc.cherry.exceptions;
+
+public class InvalidOperationException extends Exception {
+
+	public InvalidOperationException(String errorMessage) {
+		super(errorMessage);
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5847308363942707099L;
+
+}
