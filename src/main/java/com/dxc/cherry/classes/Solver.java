89916ee5190ded4fc97c3a85af1cc9f870119722
esyuleymanch <e.syuleymanchelebi@dxc.com>
2023-07-28 17:44:27 +0300

    Solver class added instead of separate classes for each operation

---
+++ src/main/java/com/dxc/cherry/classes/Solver.java
@@ -0,0 +1,18 @@
+package com.dxc.cherry.classes;
+
+public class Solver {
+	
+	public static float sum(float lhs, float rhs) {
+		return lhs + rhs;
+	}
+	public static float substract(float lhs, float rhs) {
+		return lhs - rhs;
+	}
+	public static float devide(float lhs, float rhs) {
+		return lhs * rhs;
+	}
+	public static float multiply(float lhs, float rhs) {
+		return lhs / rhs;
+	}
+
+}
--- src/main/java/com/dxc/cherry/interfaces/Operation.java
+++
@@ -1,7 +0,0 @@
-package com.dxc.cherry.interfaces;
-
-public interface Operation {
-	
-	public void  solve(float lhs, float rhs);
-
-}
