facacd81135b3888116d0f3c391269a409775ef4
Kamelia Toteva <kamelia.toteva@dxc.com>
2023-07-29 10:51:26 +0300

    Deleted useless tests, added new

--- src/test/java/com/dxc/cherry/test/CalculatorTest.java
+++ src/test/java/com/dxc/cherry/test/CalculatorTest.java
@@ -2,63 +2,54 @@ package com.dxc.cherry.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
+import com.dxc.cherry.classes.parser.Lexer;
+import com.dxc.cherry.classes.parser.Token;
 import com.dxc.cherry.exceptions.InvalidExpressionException;
 import com.dxc.cherry.exceptions.InvalidOperationException;
 
 class CalculatorTest {
 
+	/*
+	 * @Test void ExampleExpressionShouldEqual5() { String exampleExpression =
+	 * "3+5*(7-1)/(7+8)"; Calculator calc = new Calculator(); assertEquals(5,
+	 * calc.Calculate(exampleExpression)); }
+	 * 
+	 * @Test void DivOf1And0ShouldThrowArithemticException() {
+	 * assertThrows(ArithmeticException, Div::calculate(1,0)); }
+	 * 
+	 * @Test void ExampleExpressionShouldThrowInvalidOperationException() { //given
+	 * that # is not an implemented operation String exampleExpression =
+	 * "3+5*(7-1)/(7#8)"; Calculator calc = new Calculator();
+	 * assertThrows(InvalidOperationException.class,
+	 * calc.Calculate(exampleExpression)); }
+	 * 
+	 * @Test void ExampleExpressionShouldThrowInvalidExpressionException() {
+	 * //invalid bracket placement String exampleExpression = "3+5)*(7-1)/(7+8)";
+	 * Calculator calc = new Calculator();
+	 * assertThrows(InvalidExpressionException.class,
+	 * calc.Calculate(exampleExpression)); }
+	 * 
+	 * @Test void ExampleExpressionShouldThrowInvalidExpressionException() {
+	 * //missing left or right operand String exampleExpression = "3+5*(7-1)/(7+)";
+	 * Calculator calc = new Calculator();
+	 * assertThrows(InvalidExpressionException.class,
+	 * calc.Calculate(exampleExpression)); }
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	@Test
-	void SumOf2And2ShouldReturn4() {
-		assertEquals(4, Sum::calculate(2, 2));
-	}
-	@Test
-	void SubOf2And2ShouldReturn0() {
-		assertEquals(0, Sub::calculate(2, 2));
-	}
-	@Test
-	void MultOf2And2ShouldReturn4() {
-		assertEquals(4, Mult::calculate(2, 2));
-	}
-	@Test 
-	void DivOf2And2ShouldReturn1(){
-		assertEquals(1, Div::calculate(2, 2));
-	}
-	@Test
-	void ExampleExpressionShouldEqual5() {
-		String exampleExpression = "3+5*(7-1)/(7+8)";
-		Calculator calc = new Calculator();
-		assertEquals(5, calc.Calculate(exampleExpression));
-	}
-	
-	@Test
-	void DivOf1And0ShouldThrowArithemticException() {
-		assertThrows(ArithmeticException, Div::calculate(1,0));
-	}
-	@Test
-	void ExampleExpressionShouldThrowInvalidOperationException() {
-		//given that # is not an implemented operation
-		String exampleExpression = "3+5*(7-1)/(7#8)";
-		Calculator calc = new Calculator();
-		assertThrows(InvalidOperationException.class, calc.Calculate(exampleExpression));
-	}
-	@Test
-	void ExampleExpressionShouldThrowInvalidExpressionException() {
-		//invalid bracket placement
-		String exampleExpression = "3+5)*(7-1)/(7+8)";
-		Calculator calc = new Calculator();
-		assertThrows(InvalidExpressionException.class, calc.Calculate(exampleExpression));
-	}
-	@Test
-	void ExampleExpressionShouldThrowInvalidExpressionException() {
-		//missing left or right operand
-		String exampleExpression = "3+5*(7-1)/(7+)";
-		Calculator calc = new Calculator();
-		assertThrows(InvalidExpressionException.class, calc.Calculate(exampleExpression));
+	void TestTokens() throws InvalidOperationException {
+		Lexer lexer = new Lexer("5 + 4 + 7");
+		ArrayList<Token> tokens = (ArrayList<Token>) lexer.getTokens();
+		String expected = "Token{type=NUMBER, lexeme='5'}\n"
+				+ "Token{type=PLUS, lexeme='+'}\n"
+				+ "Token{type=NUMBER, lexeme='4'}\n"
+				+ "Token{type=PLUS, lexeme='+'}\n"
+				+ "Token{type=NUMBER, lexeme='7'}\n";
+		String actual = lexer.toString();
+		assertFalse(tokens.isEmpty());
+		assertEquals(expected, actual);
 	}
 }
